db98f7dfa9113a44eb44f1cb1cd908d682a5ac7c
Abdulkadir Eyigül <aeyigul@outlook.com>
2023-01-05 01:35:28 +0300

    fastify added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+// const express = require('express');
+const fs = require("fs");
+const fastify = require('fastify')({
+    logger: true,
+    https: {
+        key: fs.readFileSync("src/server.key"),
+        cert: fs.readFileSync("src/server.crt")
+    }
+})
+// const helmet = require('helmet');
+// const cors = require('cors');
 const dotenv = require("dotenv").config();
 
-const userRoutes = require('./api/v1/routes/user');
+// const userRoutes = require('./api/v1/routes/user');
 
-const app = express();
-app.use(express.json());
-app.use(helmet());
+// const app = express();
+// app.use(express.json());
+// app.use(helmet());
 
 const corsOptions = {
     credentials: true,
@@ -17,13 +25,22 @@ const corsOptions = {
     maxAge: 600,
     preflightContinue: false
 }
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
-app.use('/users', userRoutes);
-app.use((req, res, next) => {
-    res.status(404).send({error: '404 Not Found'})
-    next();
+// app.use('/users', userRoutes);
+// app.use((req, res, next) => {
+    // res.status(404).send({error: '404 Not Found'})
+    // next();
+// })
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+    reply.send({ hello: 'world' })
+})
+
+fastify.post('/', function (request, reply) {
+    reply.send({ data: request.body })
 })
 
 const PORT = process.env.PORT || 2022;
-app.listen(PORT, () => {console.log('server is running on ' + PORT)})
+fastify.listen(PORT, () => {console.log('server is running on ' + PORT)})
